415513f49abde6bf12c6e3871f3577cac4b6de07
zelen <zelen@DESKTOP-HA769PP>
2024-06-10 22:18:06 +0500

    Добавьте файлы проекта.

---
+++ TrainVvodVivod/AddPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrainVvodVivod
+{
+    /// <summary>
+    /// Логика взаимодействия для AddPage.xaml
+    /// </summary>
+    public partial class AddPage : Page
+    {
+        private Лист1_ _newZapis = new Лист1_();
+
+        
+
+        public AddPage(Лист1_ selectedZapis)
+        {
+            InitializeComponent();
+            if (selectedZapis != null)
+                _newZapis = selectedZapis;
+
+            DataContext = _newZapis;
+        }
+
+        private void AddZapBtnClick(object sender, RoutedEventArgs e)
+        {
+            TrainDemEntities2.GetContext().Лист1_.Add(_newZapis);
+            TrainDemEntities2.GetContext().SaveChanges();
+            Manager.MainFrame.Navigate(new OutputDan());
+            MessageBox.Show("Запись добавлена");
+        }
+
+        private void BackPageBtnClick(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.GoBack(); 
+        }
+    }
+}
---
+++ TrainVvodVivod/OutputDan.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TrainVvodVivod
+{
+    /// <summary>
+    /// Логика взаимодействия для OutputDan.xaml
+    /// </summary>
+    public partial class OutputDan : Page
+    {
+        public OutputDan()
+        {
+            InitializeComponent();
+
+            InfoGrd.ItemsSource = TrainDemEntities2.GetContext().Лист1_.ToList();
+        }
+
+        private void GoToAddBtnClick(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddPage(null));
+        }
+
+        private void DeleteBtnClick(object sender, RoutedEventArgs e)
+        {
+            if (InfoGrd.SelectedItems.Count != 1)
+            {
+                MessageBox.Show("Выберите строку для удаления");
+            }
+            else
+            {
+                TrainDemEntities2.GetContext().Лист1_.Remove((Лист1_)InfoGrd.SelectedItem);
+                TrainDemEntities2.GetContext().SaveChanges();
+                MessageBox.Show("Запись удалена");
+                InfoGrd.ItemsSource = TrainDemEntities2.GetContext().Лист1_.ToList();
+            }
+        }
+    }
+}
